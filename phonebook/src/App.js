8cde8436cf609aa040bde49705f982a6be2b21c2
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2022-10-19 18:24:17 +0500

    Exercise 2.9 - phonebook

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -6,18 +6,27 @@ const App = () => {
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [nameFilter, setNameFilter] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
 
   const addNewName = (e) => {
     setNewName(e.target.value);
-  }
+  };
 
   const addNewNumber = (e) => {
     setNewNumber(e.target.value);
+  };
+
+  const findPerson = (e) => {
+    setNameFilter(e.target.value);
+    const regex = new RegExp(e.target.value, 'ig');
+    const searchResult = persons.filter((person) => regex.test(person.name));
+    setSearchResult(searchResult);
   }
 
   const isPersonExist = (name) => {
     return persons.some(person => person.name === name);
-  }
+  };
 
   const addNewPerson = (e) => {
     e.preventDefault();
@@ -26,12 +35,15 @@ const App = () => {
     } else {
       alert(`${newName} is already added to phonebook`);
     }
-  }
+  };
 
-  console.log(persons);
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          filter shown with <input value={nameFilter} onChange={findPerson}/>
+        </div>
+      <h2>add a new</h2>
       <form onSubmit={addNewPerson}>
         <div>
           name: <input value={newName} onChange={addNewName}/>
@@ -44,11 +56,11 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map((person) =>
-          <li key={person.name}>{`${person.name} ${person.number}`}</li>
-        )}
-      </ul>
+      {
+        searchResult.length > 0 
+          ? searchResult.map((person) => <div key={person.name}>{`${person.name} ${person.number}`}</div>)
+          : persons.map((person) =><div key={person.name}>{`${person.name} ${person.number}`}</div>)
+      }
     </div>
   )
 }
