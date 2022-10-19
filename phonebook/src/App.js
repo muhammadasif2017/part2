c2b58bd312ea83fd0c2fa03cb9920fa4d36b57b8
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2022-10-19 17:16:10 +0500

    Exercise 2.8 - phonebook

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,12 +2,17 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' }
   ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addNewName = (e) => {
     setNewName(e.target.value);
+  }
+
+  const addNewNumber = (e) => {
+    setNewNumber(e.target.value);
   }
 
   const isPersonExist = (name) => {
@@ -17,7 +22,7 @@ const App = () => {
   const addNewPerson = (e) => {
     e.preventDefault();
     if (!isPersonExist(newName)) {
-      setPersons([...persons, { name: newName }]);
+      setPersons([...persons, { name: newName, number: newNumber }]);
     } else {
       alert(`${newName} is already added to phonebook`);
     }
@@ -32,13 +37,16 @@ const App = () => {
           name: <input value={newName} onChange={addNewName}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={addNewNumber}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) =>
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>{`${person.name} ${person.number}`}</li>
         )}
       </ul>
     </div>
