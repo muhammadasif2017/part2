1d85f7ef2c85b30827fa259d7366145d926f0e91
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2022-10-21 17:22:10 +0500

    Change api url and resolve css issue

--- phonebook/src/services/phonebook.js
+++ phonebook/src/services/phonebook.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/persons';
+const baseUrl = 'https://asif-node.herokuapp.com/';
 
 const getAllPersons = () => {
   const request = axios.get(baseUrl)
